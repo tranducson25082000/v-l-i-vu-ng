1e7c84596bbb061e7c3e8c0eefd72913adbeafcf
tranducson25082000 <48961012+tranducson25082000@users.noreply.github.com>
2019-05-30 23:51:01 +0700

    Update one turtle go to goals .cpp

--- one turtle go to goals .cpp
+++ one turtle go to goals .cpp
@@ -1,5 +1,3 @@
-
-
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <turtlesim/Pose.h>
@@ -11,7 +9,10 @@ using namespace std;
 
 ros::Publisher pub;
 const float PI = 3.14159265;
+const double tolerance =1e-3;//pow(10,-8);
+bool firstTime=true;
 float rate = 100;//600;
+
 turtlesim::Pose current_pose;
 
 geometry_msgs::Twist getMessage(double linear_x, double angular_z)
@@ -25,6 +26,9 @@ geometry_msgs::Twist getMessage(double linear_x, double angular_z)
 void poseCallback(const turtlesim::Pose::ConstPtr& msg)
 {
     current_pose = *msg;
+    firstTime=false;
+    
+   
 }
 
 int main(int argc, char** argv)
@@ -35,39 +39,44 @@ int main(int argc, char** argv)
      pub = h.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1000);
      ros::Subscriber sub =h.subscribe("/turtle1/pose", 1000, poseCallback);
      ros::Rate loopRate(rate);
-    //  cout<<"argc=:"<<argc;
+     cout<<"argc=:"<<argc;
      double t0= ros::Time::now().toSec();
      double a;
      for(int i=1;i<argc;i+=2)
-     {
+     {   
          double x0=atof(argv[i]);
          double y0=atof(argv[i+1]);
-         const double tolerance =1e-3;//pow(10,-8);
-        //  cout<<"argv:"<<argv;
+     
+         cout<<"argv:"<<argv;
          while (ros::ok())
           {
              loopRate.sleep();
              ros::spinOnce();
+             if(!firstTime)
+             {
              cout <<fixed<<setprecision(2)<< current_pose.x << " " << current_pose.y << " " << current_pose.theta << endl;
              double distance = sqrt( pow(x0-current_pose.x, 2) + pow(y0-current_pose.y, 2) );
-             if (distance < tolerance) {
            
+             if (distance < tolerance) { 
                 pub.publish(getMessage(0,0));
                 double t1= ros::Time::now().toSec();
                 cout <<"Thời gian đi hết qua tất cả các điểm là : " << t1 -t0 << endl;
                 break;
-            
              }
-         
+             
              double dx = x0 - current_pose.x, dy = y0 - current_pose.y, theta = current_pose.theta;
-             double dalpha = asin ((cos(theta)*dy-sin(theta)*dx) / distance);
+          
              double angle  = acos ((cos(theta)*dx+sin(theta)*dy) / distance);
              
              if (angle < PI/2) {  a = 1;}
              else a=-1;
-             pub.publish( getMessage( a*min(100*distance, 50.0), a*50*dalpha ));//10,10.0,35;
+             double dalpha = asin((cos(theta) * dy- sin(theta) * dx) / distance);
+            
+             pub.publish( getMessage( a*min(18*distance, 20.0), a*20*dalpha ));//10,10.0,35;
+            
+             }
           }
-     }
+          }
+     
 return 0;
 }
-
